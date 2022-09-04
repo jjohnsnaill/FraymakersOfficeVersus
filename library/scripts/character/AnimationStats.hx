c843950050d09ff9a3e2165f2f8207f2786f0c3b
John Snail <54149393+jjohnsnaill@users.noreply.github.com>
2022-09-04 22:25:27 +0100

    0.1

---
+++ library/scripts/character/AnimationStats.hx
@@ -0,0 +1,15 @@
+{
+	stand: {},
+	walkin: {},
+	walk: { influenceMultiplier: 1 },
+	walkout: {},
+	run: { influenceMultiplier: 1 },
+	crouch: {},
+	jumpsquat: {},
+	jump: { allowFastFall: true, influenceMultiplier: 1 },
+	land: {},
+	hurt: {},
+	attack: { allowTurn: true },
+	aerial: { influenceMultiplier: 1 },
+	charge: { chargeFramesMax: 120, endType: AnimationEndType.LOOP }
+}
---
+++ library/scripts/character/HitboxStats.hx
@@ -0,0 +1,20 @@
+{
+	stand: {},
+	walkin: {},
+	walk: {},
+	walkout: {},
+	run: {},
+	crouch: {},
+	jumpsquat: {},
+	jump: {},
+	land: {},
+	hurt: {},
+	attack: {
+		hitbox0: { damage: 5, angle: 75, baseKnockback: 40, knockbackGrowth: 50, hitstop: 10, limb: AttackLimb.SWORD }
+	},
+	aerial: {
+		hitbox0: { damage: 2, angle: 45, baseKnockback: 20, knockbackGrowth: 20, hitstop: 5, limb: AttackLimb.BODY },
+		hitbox1: { damage: 3, angle: 45, baseKnockback: 30, knockbackGrowth: 50, hitstop: 5, limb: AttackLimb.BODY }
+	},
+	charge: {}
+}
